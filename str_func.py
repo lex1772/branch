0140ef1945dfd2e0273e401092d62fe2f1632486
lexa1 <lexa1772@gmail.com>
2023-02-10 16:52:52 +0300

    str_func.py updated

--- str_func.py
+++ str_func.py
@@ -1,2 +1,11 @@
 def string_upper(string):
+    '''Функция, которая делает заглавной первую букву'''
     return string.upper()
+
+def upper_2(string):
+    '''Функция, которая делает заглавными первые буквы каждого слова в строке'''
+    string_list = string.split()
+    new_string = []
+    for i in string_list:
+        new_string.append(i.capitalize())
+    return " ".join(new_string)
